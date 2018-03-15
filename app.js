253b94dee8a574b49611fd046a5c29d05fc14664
Jordon Frank <Maudemoo@gmail.com>
2018-03-15 13:17:33 -0700

    added potention loops for logos

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 
-function sendError() {
-   document.getElementById('img-url').value = alert('Please enter URL')
-}
-
 document.getElementById('addPost').addEventListener('click', addPost
 )
 function addPost(e){
   e.preventDefault();
   let img = document.getElementById('img-url').value || sendError()
-
   fetch('https://dev.sighthoundapi.com/v1/recognition?objectType=vehicle', {
     method:'POST',
     headers: {
@@ -23,32 +18,69 @@ function addPost(e){
     document.getElementById('#')
     let carUrl = document.getElementById('carUrl')
     let cars = data.objects
-
-
+    document.getElementById("parent").innerHTML = '';
     for( let i = 0; i < cars.length; i++){
       let color = cars[i].vehicleAnnotation.attributes.system.color.name;
       let make = cars[i].vehicleAnnotation.attributes.system.make.name;
       let model = cars[i].vehicleAnnotation.attributes.system.model.name;
       let con = cars[i].vehicleAnnotation.recognitionConfidence;
       let objects = document.createElement("div")
+
       console.log(color)
       objects.classList.add("car")
       objects.classList.add("col-md-4")
+      objects.setAttribute("id", "car")
       objects.innerHTML = `${color.toUpperCase()}, ${make}, ${model}, with a confidence rating (${con})`
       document.getElementById("parent").appendChild(objects)
 
+
+
       }
+      // for(let i = 0; i < cars.length; i++){
+      //   let make = cars[i].vehicleAnnotation.attributes.system.make.name;
+      //   let objects = document.createElement("div")
+      //   objects.classList.add("logo")
+      //   objects.classList.add("col-md-4")
+      //   objects.setAttribute("id", "logo")
+      //   document.getElementById("parent").appendChild(objects)
+      //
+      // }
+
+    // for(let i = 0; i < cars.length; i++){
+    //
+    //
+    //   let logo = make.replace(/\s/g, "").toUpperCase()
+    //   let logo_container = `<div id="logo-container"class="col-md-4 logo-container"><img id="logo${i}" class="logo" src="images/${logoId}">`
+    //     document.getElementById('logo-container').innerHTML += logo_container;
+    //   }
 
       openNav()
+
+
 
 
   })
 }
 
+function clear(){
+  let node = document.getElementByClassName("car");
+    if (node.parentNode) {
+      node.parentNode.removeChild(node);
+}
+
+
+}
+
+
+function sendError() {
+   document.getElementById('img-url').value = alert('Please enter URL')
+}
 
 function openNav() {
+
     document.getElementById("myNav").style.width = "100%";
     document.getElementById("carID").src = document.getElementById('img-url').value;
+
 
 }
 
@@ -57,6 +89,3 @@ function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 
 }
-
-
-// http://www.gmc.com/content/dam/gmc/na/us/english/index/vehicles/2018/trucks/sierra-1500-mov/01-images/2018-sierra1500-mov-denali-onyx-18PGSR00335.jpg?imwidth=1200
